e0c807f7ee24ef197a41f4ecbcc61b57774a87af
SteelMan <concetto.pollicina@gmail.com>
2018-12-30 10:51:05 +0100

    aggiunto supporto i18n pipe a cascata

--- src/app/pipes/i18n.pipe.ts
+++ src/app/pipes/i18n.pipe.ts
@@ -8,14 +8,32 @@ import { I18nService } from '../services/i18n.service';
 export class I18nPipe
   implements PipeTransform
 {
+  private static MAX_CHAIN_LENGTH: number = 5;
+  private static current_ring: number;
 
   constructor(
     protected i18n: I18nService
   ) {
   }
 
+  private translate(val: string): string {
+    let trans = this.i18n.phrase(val);
+    if (I18nPipe.current_ring < I18nPipe.MAX_CHAIN_LENGTH) {
+      const search: string = ' | i18n';
+      const pipePos: number = trans.lastIndexOf(search);
+      if (pipePos !== -1 && pipePos === trans.length - search.length) {
+        trans = this.translate(trans.substr(0, pipePos));
+      }
+      ++I18nPipe.current_ring;
+    } else {
+      console.error('MAX_CHAIN_LENGTH exceeded');
+    }
+    return trans;
+  }
+
   transform(value: string): string
   {
-    return this.i18n.phrase(value);
+    I18nPipe.current_ring = 0;
+    return this.translate(value);
   }
 }
